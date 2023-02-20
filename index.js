cc2533eaa0691e21a6e0c19287761c0196260364
YsisC <ysisclongart@gmail.com>
2023-02-20 13:20:06 -0300

    Config Jest, babel, and export and import

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks= (path, options)=>{
+return new Promise((resolve, reject) => {
+  //Identifica si la ruta existe
+if (fs.existsSync(path)) {
+   //Chequear o converir a una ruta absoluta
+} else{
+  //Sino existe la ruta
+  reject('la ruta no existe')
+}
+})
+};
+module.exports = {
+mdLinks,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,16 @@
-const mdLinks = require('../');
-
+const {mdLinks }= require('../index.js')
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
+  // it('should...', () => {
+  //   console.log('FIX ME!');
+  // });
+//   it('should return a new promise.', () => {
+//  expect(mdLinks()).toBe( typeof Promise)
+//   });
+  it('should reject when a path doesnt exits.', () => {
+    return mdLinks("/ruta/doesntexist.md").catch((error)=>{
+    expect(error).toBe('la ruta no existe');
+    })
+     });
 });
